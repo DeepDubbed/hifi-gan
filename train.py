24a9dc23fbb0aa00e8b1bf330869e5d0c6bfcb0a
Mihai Cvasnievschi <mihai.cvasnievschi@gmail.com>
2021-03-22 20:34:25 +0200

    Fix cache

--- train.py
+++ train.py
@@ -98,7 +98,7 @@ def train(rank, a, h):
 
     if rank == 0:
         validset = MelDataset(validation_filelist, h.segment_size, h.n_fft, h.num_mels,
-                              h.hop_size, h.win_size, h.sampling_rate, h.fmin, h.fmax, False, False, n_cache_reuse=0,
+                              h.hop_size, h.win_size, h.sampling_rate, h.fmin, h.fmax, False, False,
                               num_mels_loss=h.num_mels_loss, fmax_loss=h.fmax_for_loss,
                               device=device, fine_tuning=a.fine_tuning,
                               base_mels_path=a.input_mels_dir,
